cc55fd6a006516fb800a397a394f0be9927a2987
Alon <ackeralon3@gmail.com>
2026-06-17 00:23:27 +0300

    Make Richard real AI: ban emojis/markdown in prompts, fix live frontend to load app same-origin, remove debug endpoint

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({
-    hasKey: !!process.env.ANTHROPIC_API_KEY,
-    projectName: process.env.VERCEL_PROJECT_NAME,
-    projectUrl: process.env.VERCEL_PROJECT_PRODUCTION_URL,
-    env: process.env.VERCEL_ENV
-  });
-};
